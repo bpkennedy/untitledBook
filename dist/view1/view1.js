00acd8ed785ea1aa68dcc65652ea9a283eadbe3c
Brian <bpkennedy@gmail.com>
2015-07-12 21:43:12 -0500

    comment

--- dist/view1/view1.js
+++ dist/view1/view1.js
@@ -9,7 +9,7 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function($scope, githubHistoryService) {
+.controller('View1Ctrl', ['$scope', 'githubHistoryService', function($scope, githubHistoryService) {
 
   $scope.gitData = "";
 
