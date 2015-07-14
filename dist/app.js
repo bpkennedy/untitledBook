f8970ae841168c6e1a7f3c301ee53db19a50a175
Brian <bpkennedy@gmail.com>
2015-07-13 21:42:22 -0500

    github service working!  Now adding binding to the view.

--- dist/app.js
+++ dist/app.js
@@ -11,26 +11,10 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]).
 service('githubHistoryService', function($http) {
-
-  /*var commitData = "";
-
-  var getGithubData = function() {
-    $http.get('https://api.github.com/repos/bpkennedy/untitledBook/commits').
-      success(function (data, status, headers, config) {
-        commitData = data;
-        console.log(commitData);
-        return commitData;
-      }).
-      error(function (data, status, headers, config) {
-        console.log("There was some error in the http GET call to github.");
-      });
-  };*/
-
   return {
     getGithubData: function() {
       var url = "https://api.github.com/repos/bpkennedy/untitledBook/commits";
       return $http.get(url);
     }
   };
-
 });
